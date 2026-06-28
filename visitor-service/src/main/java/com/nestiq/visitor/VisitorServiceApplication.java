161f906b02c3e30932ecb4b24e2c2628ac826a19
sp_rath1310 <rath13101999@gmail.com>
2026-06-29 00:59:31 +0530

    updated flow

---
+++ visitor-service/src/main/java/com/nestiq/visitor/VisitorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nestiq.visitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorServiceApplication.class, args);
+	}
+
+}
---
+++ visitor-service/src/test/java/com/nestiq/visitor/VisitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestiq.visitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
